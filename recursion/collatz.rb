9e18bfed66e6b5117600d8aecddba6ba642411e3
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-14 09:46:46 -0600

    Add: Collatz exercise from Harvard CS50

---
+++ recursion/collatz.rb
@@ -0,0 +1,19 @@
+def collatz(n)
+  return 0 if n == 1
+
+  return 1 + collatz(n / 2) if n % 2 == 0
+
+  return 1 + collatz(3 * n + 1) if n % 2 == 1
+end
+
+puts collatz(1)
+puts collatz(2)
+puts collatz(3)
+puts collatz(4)
+puts collatz(5)
+puts collatz(6)
+puts collatz(7)
+puts collatz(8)
+puts collatz(15)
+puts collatz(27)
+puts collatz(50)
