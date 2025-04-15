75d1dbdb7c6ee5ca9e5e5e638e9ca892f968224a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-15 09:32:36 -0600

    Update: Print to show the array, not each digit

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -24,5 +24,5 @@ def fibs_rec(n)
   end
 end
 
-puts fibs(8) # [0, 1, 1, 2, 3, 5, 8, 13]
-puts fibs_rec(8) # [0, 1, 1, 2, 3, 5, 8, 13]
+print fibs(8) # [0, 1, 1, 2, 3, 5, 8, 13]
+print fibs_rec(8) # [0, 1, 1, 2, 3, 5, 8, 13]
